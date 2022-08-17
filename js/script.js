1110b0444a6478cfd6c97365a2a8fde7a7670c14
jim <jimdouglasmmferreira@gmail.com>
2022-08-17 20:20:10 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+class Produto{
+
+    constructor(){
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+
+    }
+    adicionar(){
+        alert('cliente cadastrado!');
+        //...
+    }
+    
+    excluir(){
+        //...
+
+        alert('cliente deletado!');
+    }
+}
+
+var produto = new Produto();
