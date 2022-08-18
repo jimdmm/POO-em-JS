c1dd7baba94b2c28a524726592175827eacbe49b
jimdmm <94024972+jimdmm@users.noreply.github.com>
2022-08-18 08:32:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,87 @@
-class Produto{
+class Cliente{
 
     constructor(){
-        this.id = 0;
-        this.nomeProduto = '';
-        this.valor = 0;
+        this.id = 1;
+        this.arrayCliente = [];
 
-    }
-    adicionar(){
-        alert('cliente cadastrado!');
-        //...
     }
     
-    excluir(){
-        //...
+    salvar(){
+        let cliente = this.lerDados();
 
-        alert('cliente deletado!');
+        if(this.validaCampos(cliente)){
+            this.adicionar(cliente);
+        }
+
+        this.listaTabela();
+
+    }
+
+    listaTabela() {
+        let tbody = document.getElementById('tbody');
+
+        for(let i = 0; i < this.arrayCliente.length; i++ ) {
+            let tr = tbody.insertRow();
+            
+            let td_id = tr.insertCell();
+            let td_nome = tr.insertCell();
+            let td_cpf = tr.insertCell();
+            let td_genero = tr.insertCell();
+            let td_telefone = tr.insertCell();
+            let td_endereco = tr.insertCell();
+            let td_observacoes = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+        }
+    }
+  
+    adicionar(cliente){
+        this.arrayCliente.push(cliente);
+        this.id++;
+    }
+
+    lerDados(){
+        let cliente ={}
+
+        cliente.id = this.id;
+        cliente.nomeCompleto = document.getElementById('nome').value;
+        cliente.cpf = document.getElementById('cpf').value;
+        cliente.genero = document.getElementById('genero').value;
+        cliente.telefone = document.getElementById('telefone').value;
+        cliente.endereco = document.getElementById('endereco').value;
+        cliente.observacoes = document.getElementById('observacoes').value;
+        
+        return cliente;
+    }
+
+    validaCampos(cliente) {
+        let msg = '';
+
+        if(cliente.nomeCompleto == ''){
+            msg += '- Informe o nome do Cliente! \n';
+        }
+
+        if(cliente.cpf == ''){
+            msg += '- Informe o seu CPF! \n';
+        }
+
+        if(cliente.telefone == ''){
+            msg += '- Informe o número de Telefone! \n';
+        }
+
+        if(cliente.endereco == ''){
+            msg += '- Informe o seu Endereço! \n';
+        }
+        
+        if(msg != ''){
+            alert(msg);
+            return false;
+        }
+        return true;
+    }
+
+    cancelar(){
     }
 }
 
-var produto = new Produto();
+var cliente = new Cliente();
