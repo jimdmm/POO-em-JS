4198c3d7f8ab0208893e780df50563ad21d616ce
jimdmm <94024972+jimdmm@users.noreply.github.com>
2022-08-19 07:59:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
-class Cliente{
+class Cliente {
 
-    constructor(){
+    constructor() {
         this.id = 1;
         this.arrayCliente = [];
-
+        this.editId = null;
     }
-    
-    salvar(){
+
+    salvar() {
         let cliente = this.lerDados();
 
-        if(this.validaCampos(cliente)){
-            this.adicionar(cliente);
+        if (this.validaCampos(cliente)) {
+            if (this.editId == null) {
+                this.adicionar(cliente);
+            } else {
+                this.atualizar(this.editId, cliente);
+             }
+
         }
 
         this.listaTabela();
-
+        this.cancelar();
     }
 
     listaTabela() {
         let tbody = document.getElementById('tbody');
         tbody.innerText = '';
 
-        for(let i = 0; i < this.arrayCliente.length; i++ ) {
+        for (let i = 0; i < this.arrayCliente.length; i++) {
             let tr = tbody.insertRow();
-            
+
             let td_id = tr.insertCell();
             let td_nome = tr.insertCell();
             let td_cpf = tr.insertCell();
@@ -40,18 +45,56 @@ class Cliente{
             td_telefone.innerText = this.arrayCliente[i].telefone;
             td_endereco.innerText = this.arrayCliente[i].endereco;
             td_observacoes.innerText = this.arrayCliente[i].observacoes;
-            
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'img/edit.png';
+            imgEdit.setAttribute("onclick", "cliente.preparaEditacao(" + JSON.stringify(this.arrayCliente[i]) + ")")
+
+            let imgDelete = document.createElement('img');
+            imgDelete.src = 'img/delete.png';
+            imgDelete.setAttribute("onclick", "cliente.deletar(" + this.arrayCliente[i].id + ")")
+
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelete);
+
+
 
         }
     }
-  
-    adicionar(cliente){
+
+    adicionar(cliente) {
         this.arrayCliente.push(cliente);
         this.id++;
     }
 
-    lerDados(){
-        let cliente ={}
+    atualizar(id, cliente) {
+        for (let i = 0; i < this.arrayCliente.length; i++) {
+            if(this.arrayCliente[i].id == id){
+                this.arrayCliente[i].nomeCompleto = cliente.nomeCompleto;
+                this.arrayCliente[i].cpf = cliente.cpf;
+                this.arrayCliente[i].genero = cliente.genero;
+                this.arrayCliente[i].telefone = cliente.telefone;
+                this.arrayCliente[i].endereco = cliente.endereco;
+                this.arrayCliente[i].observacoes = cliente.observacoes;
+            }
+        }
+    }
+
+    preparaEditacao(dados) {
+        this.editId = dados.id;
+
+        document.getElementById('nome').value = dados.nomeCompleto;
+        document.getElementById('cpf').value = dados.cpf;
+        document.getElementById('genero').value = dados.genero;
+        document.getElementById('telefone').value = dados.telefone;
+        document.getElementById('endereco').value = dados.endereco;
+        document.getElementById('observacoes').value = dados.observacoes;
+
+        document.getElementById('btn1').innerText = 'Atualizar';
+    }
+
+    lerDados() {
+        let cliente = {}
 
         cliente.id = this.id;
         cliente.nomeCompleto = document.getElementById('nome').value;
@@ -60,37 +103,60 @@ class Cliente{
         cliente.telefone = document.getElementById('telefone').value;
         cliente.endereco = document.getElementById('endereco').value;
         cliente.observacoes = document.getElementById('observacoes').value;
-        
+
         return cliente;
     }
 
     validaCampos(cliente) {
         let msg = '';
 
-        if(cliente.nomeCompleto == ''){
+        if (cliente.nomeCompleto == '') {
             msg += '- Informe o nome do Cliente! \n';
         }
 
-        if(cliente.cpf == ''){
+        if (cliente.cpf == '') {
             msg += '- Informe o seu CPF! \n';
         }
 
-        if(cliente.telefone == ''){
+        if (cliente.telefone == '') {
             msg += '- Informe o número de Telefone! \n';
         }
 
-        if(cliente.endereco == ''){
+        if (cliente.endereco == '') {
             msg += '- Informe o seu Endereço! \n';
         }
-        
-        if(msg != ''){
+
+        if (msg != '') {
             alert(msg);
             return false;
         }
         return true;
     }
 
-    cancelar(){
+    cancelar() {
+        document.getElementById('nome').value = '';
+        document.getElementById('cpf').value = '';
+        document.getElementById('telefone').value = '';
+        document.getElementById('endereco').value = '';
+        document.getElementById('observacoes').value = '';
+
+        document.getElementById('btn1').innerText = 'Salvar';
+        this.editId = null;
+    }
+
+    deletar(id) {
+
+        if (confirm('Deseja realmente deletar o cliente' + id)) {
+            let tbody = document.getElementById('tbody');
+
+            for (let i = 0; i < this.arrayCliente.length; i++) {
+                if (this.arrayCliente[i].id == id) {
+                    this.arrayCliente.splice(i, 1);
+                    tbody.deleteRow(i);
+                }
+            }
+        }
+
     }
 }
 
